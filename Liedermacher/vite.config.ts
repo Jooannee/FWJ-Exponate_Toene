0f41d31b5a581d7c2f738372874ffc8c9bc86977
Jooannee <waddermelone0@gmail.com>
2024-09-19 11:16:51 +0200

    Added specific ports to each vite config

--- Liedermacher/vite.config.ts
+++ Liedermacher/vite.config.ts
@@ -4,6 +4,9 @@ import electron from "vite-plugin-electron";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 4003
+  },
   plugins: [
     react(),
     electron({
--- Töne-Hannovers/vite.config.ts
+++ Töne-Hannovers/vite.config.ts
@@ -4,6 +4,9 @@ import electron from "vite-plugin-electron";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 4005
+  },
   plugins: [
     react(),
     electron({
