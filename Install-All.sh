c2f7591f71cab6c9f8e64a32e9709cce91486e59
Jooannee <waddermelone0@gmail.com>
2024-10-18 09:21:38 +0200

    Also move error output to file

--- Install-All.sh
+++ Install-All.sh
@@ -8,36 +8,36 @@ cd "$(dirname "$0")"
 
 echo $(date +%d%t%b%t%Y%t%T%t%:z) > Install.log
 
-npm install --global yarn >> Install.log
-npm install --global @yao-pkg/pkg >> Install.log
-npm install --global electron >> Install.log
-npm install --global ws >> Install.log
-npm install --global concurrently >> Install.log
+npm install --global yarn >> Install.log 2>&1
+npm install --global @yao-pkg/pkg >> Install.log 2>&1
+npm install --global electron >> Install.log 2>&1
+npm install --global ws >> Install.log 2>&1
+npm install --global concurrently >> Install.log 2>&1
 
 
 cd Hörbeeinträchtigungen
-yarn >> Install.log
+yarn >> Install.log 2>&1
 
 cd ../Hörtest
-yarn >> Install.log
+yarn >> Install.log 2>&1
 
 cd ../Instrumenten-Ratespiel
-yarn >> Install.log
+yarn >> Install.log 2>&1
 
 cd ../Liedermacher
-yarn >> Install.log
+yarn >> Install.log 2>&1
 
 cd ../McGurk-Effekt
-yarn >> Install.log
+yarn >> Install.log 2>&1
 
 cd ../Töne-Hannovers
-yarn >> Install.log
+yarn >> Install.log 2>&1
 
 cd ../statistics-server
-yarn >> Install.log
+yarn >> Install.log 2>&1
 
 cd ../Dash
-npm link ws >> Install.log
+npm link ws >> Install.log 2>&1
 
 sudo chown root:root node_modules/electron/dist/chrome-sandbox
 sudo chmod 4755 node_modules/electron/dist/chrome-sandbox
